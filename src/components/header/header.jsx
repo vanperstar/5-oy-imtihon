c819669b49e37b081e2fb5fb106f9e1e4d19d624
v@nperstar <102234460+vanperstar@users.noreply.github.com>
2022-08-10 03:36:26 +0500

    react exam

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,17 +3,28 @@ import bourLogo from '../../assets/img/bobur-logo-header.png'
 import search from '../../assets/img/search.png'
 import { Link, NavLink } from 'react-router-dom'
 import Modal from '../modal/modal.jsx'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
     const [open, setOpen] = useState(false)
+
+    const [show, setShow] = useState(false)
+    function toggleActive(){
+        setShow(!show)
+    }
+
+    // const [head, setHead] = useState(false)
+    // function toggleActive(){
+    //     setHead(!head)
+    // }
+
     return(
         <>
-            <header className="header container">
+            <header className={`${show ? 'header header--active' : 'header container'}`}>
                 <Link to='/'>
-                    <img src={bourLogo} alt="bobur-logo" />
+                    <img className='header-logo' src={bourLogo} alt="bobur-logo" />
                 </Link>
-                <ul className="header-items">
+                <ul className={`${show ? 'header-items header-items--active' : 'header-items'}`} onClick={toggleActive}>
                     <li>
                         <Link className='header-item' to='/notfount'>All</Link>
                     </li>
@@ -40,7 +51,7 @@ const Header = () => {
                 <button className='modal-open' title='Add-post' onClick={setOpen.bind(null, true)}>Modal</button>
                 <input type="search" name="search" placeholder='Search' />
                 <button className='search-btn' type="submit"><img src={search} alt="" /></button>
-                <div class="toggle">
+                <div className={`${show ?  'toggle toggle--active' : 'toggle'}`} onClick={toggleActive}>
                     <span></span>
                 </div>
             </header>
